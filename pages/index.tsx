3076a83da36d28300d01c585feea75e7f093377a
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-03-07 08:24:13 -0300

    chore: final skill component and add projects section

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Head from "next/head";
 import About from "@/components/About"
 import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
+import Projects from "@/components/Projects";
 
 export default function Home() {
   return (
@@ -34,6 +35,9 @@ export default function Home() {
         <Skills />
       </section>
       {/* projects */}
+      <section id="projects" className="snap-center">
+        <Projects />
+      </section>
 
       {/* Contact Me */}
     </div>
