f3956f2793bc610ef1fae86807d05ab563b30c84
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-03-08 09:40:22 -0300

    feat: add initial ContactMe component

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import About from "@/components/About"
 import WorkExperience from "@/components/WorkExperience";
 import Skills from "@/components/Skills";
 import Projects from "@/components/Projects";
+import ContactMe from "@/components/ContactMe";
 
 export default function Home() {
   return (
@@ -40,6 +41,9 @@ export default function Home() {
       </section>
 
       {/* Contact Me */}
+      <section id="contact" className="snap-center">
+        <ContactMe />
+      </section>
     </div>
   );
 }
