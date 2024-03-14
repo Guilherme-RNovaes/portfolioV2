65eedb615b234dd42ed61e4d00544e4bc5f325d9
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-03-14 07:43:17 -0300

    refactor: skill component refactor, reduce code repeat

--- app/technologies/_components/Skills.tsx
+++ app/technologies/_components/Skills.tsx
@@ -5,41 +5,34 @@ import { frontEnd, backEnd, dataBase } from "@/config/skills";
 import { Badge } from "@/app/_components/ui/badge";
 import { motion } from "framer-motion";
 
+type Skill = {
+  icon: React.ElementType;
+  skill: string;
+};
+
 type Props = {}
 
 export default function Skills({ }: Props) {
-  const front = frontEnd.map((props) => {
-    return (
-      <div className="m-2">
-        <Badge variant='secondary' className="flex flex-row items-center justify-start w-36 md:w-44 h-14 gap-2 px-4 py-2 rounded-xl">
-          <props.icon size={22} />
-          <p className="text-sm font-semibold">{props.skill}</p>
-        </Badge>
+  const renderSkills = (skills: Skill[], title: string, delay: number) => (
+    <motion.div
+      initial={{ x: -50 }}
+      animate={{ x: 0 }}
+      transition={{ duration: delay }}
+      className="flex flex-col flex-wrap mb-8 items-center justify-center"
+    >
+      <h1 className="text-2xl font-semibold pb-4 px-4">{title}</h1>
+      <div className="flex flex-row flex-wrap items-center justify-center">
+        {skills.map((props, index) => (
+          <div key={index} className="m-2">
+            <Badge variant='secondary' className="flex flex-row items-center justify-start w-36 md:w-44 h-14 gap-2 px-4 py-2 rounded-xl">
+              <props.icon size={22} />
+              <p className="text-sm font-semibold">{props.skill}</p>
+            </Badge>
+          </div>
+        ))}
       </div>
-    )
-  })
-
-  const back = backEnd.map((props) => {
-    return (
-      <div className="m-2">
-        <Badge variant='secondary' className="flex flex-row items-center justify-start w-36 md:w-44 h-14 gap-2 px-4 py-2 rounded-xl">
-          <props.icon size={22} />
-          <p className="text-sm font-semibold">{props.skill}</p>
-        </Badge>
-      </div>
-    )
-  })
-
-  const data = dataBase.map((props) => {
-    return (
-      <div className="m-2">
-        <Badge variant='secondary' className="flex flex-row items-center justify-start w-36 md:w-44 h-14 gap-2 px-4 py-2 rounded-xl">
-          <props.icon size={22} />
-          <p className="text-sm font-semibold">{props.skill}</p>
-        </Badge>
-      </div>
-    )
-  })
+    </motion.div>
+  );
 
   return (
     <motion.div
@@ -48,33 +41,9 @@ export default function Skills({ }: Props) {
       transition={{ duration: 0.8 }}
       className="flex flex-col mt-24 mx-4 max-w-4xl items-center md:items-start justify-center"
     >
-      <h1 className="text-2xl font-semibold pb-4 px-4">Front-end</h1>
-      <motion.div
-        initial={{ x: -50 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 0.2 }}
-        className="flex flex-row flex-wrap mb-8 items-center justify-center"
-      >
-        {front}
-      </motion.div>
-      <h1 className="text-2xl font-semibold pb-4 px-4">Back-end</h1>
-      <motion.div
-        initial={{ x: -50 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 0.4 }}
-        className="flex flex-row flex-wrap mb-8 items-center justify-center"
-      >
-        {back}
-      </motion.div>
-      <h1 className="text-2xl font-semibold pb-4 px-4">Data-base</h1>
-      <motion.div
-        initial={{ x: -50 }}
-        animate={{ x: 0 }}
-        transition={{ duration: 0.6 }}
-        className="flex flex-row flex-wrap mb-8 items-center justify-center"
-      >
-        {data}
-      </motion.div>
+      {renderSkills(frontEnd, "Front-end", 0.2)}
+      {renderSkills(backEnd, "Back-end", 0.4)}
+      {renderSkills(dataBase, "Data-base", 0.6)}
     </motion.div>
-  )
+  );
 }
