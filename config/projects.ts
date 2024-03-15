60291b1369c96072ebf463d7796e932ac4d5ae41
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-03-15 17:12:19 -0300

    feat: create initial projects array .ts

---
+++ config/projects.ts
@@ -0,0 +1,17 @@
+export interface project {
+  name: string;
+  description: string;
+  stacks: string[];
+  link: string;
+  img: string;
+}
+
+export const projectProps: project[] = [
+  {
+    name: '',
+    description: '',
+    stacks: ['', '', ''],
+    link: '',
+    img: ''
+  },
+]
