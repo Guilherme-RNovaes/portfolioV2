ef7521bbedc63c1df761d3b34981419bcc69042c
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-03-18 17:50:15 -0300

    fix: Projects area bug fixed

--- app/(home)/_components/ProjectCard.tsx
+++ app/(home)/_components/ProjectCard.tsx
@@ -1,5 +1,8 @@
+"use client"
+
 import { Button } from "@/app/_components/ui/button";
 import { projectProps } from "@/config/projects";
+import { Github, Link2 } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
@@ -15,18 +18,27 @@ type Props = {
 const ProjectCard = ({ direction, name, img, description, link }: Props) => {
   const isRightDirection = direction === 'right'
   return (
-    <div className="px-8 flex flex-col items-center justify-center">
-      <h1 className="pb-4 font-semibold text-xl">{name}</h1>
-      <div className={`flex flex-row${isRightDirection ? '-reverse' : ''} items-center justify-center`}>
-        <Image src={img} width={280} height={280} alt="project image" className="object-cover"></Image>
-        <div className="flex flex-col items-center justify-between">
-          <p className="text-sm text-zinc-400">{description}</p>
+    <div className={`flex flex-col ${isRightDirection ? 'md:flex-row-reverse' : 'md:flex-row'} items-center justify-center mb-8`}>
+      <Image src={img} width={280} height={280} alt="project image" className="object-cover px-4 pb-8"></Image>
+      <div className={`flex flex-col items-center ${isRightDirection ? 'md:items-start' : 'md:items-end md:text-right'}`}>
+        <h1 className="pb-4 font-semibold text-xl">{name}</h1>
+        <p className="text-sm text-zinc-400 pb-8">{description}</p>
+        <div className="flex flex-row gap-4">
           <Button asChild variant='outline'>
-            <Link href={link}>Visit Page</Link>
+            <Link href={link} className="gap-2">
+              <Link2 size={18} />
+              Visit Page
+            </Link>
+          </Button>
+          <Button asChild variant='outline'>
+            <Link href={link} className="gap-2">
+              <Github size={18} />
+              Source code
+            </Link>
           </Button>
         </div>
       </div>
-    </div>
+    </div >
   )
 }
 
