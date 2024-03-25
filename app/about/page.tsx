e9e00cb37f01fa05deba899fe0935d55f7f54ae3
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-03-25 19:46:00 -0300

    feat: add initial About page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import React from "react";
 
 type Props = {}
 
-export default function About() {
+export default function Projects() {
   return (
-    <div>about</div>
+    <div className="md:ml-64 xl:ml-68 flex flex-col items-center justify-center z-0 scroll-smooth">
+      <h1>Sobre Mim</h1>
+    </div>
   )
 }
