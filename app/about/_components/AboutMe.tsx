2fcf483beffe4c8094ab5163a555983bee1ef224
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-03-29 15:50:21 -0300

    style: config animations for about page

--- app/about/_components/AboutMe.tsx
+++ app/about/_components/AboutMe.tsx
@@ -8,6 +8,25 @@ type Props = {}
 
 export default function AboutMe({ }: Props) {
 
+  const item = {
+    hidden: { opacity: 0, x: -50 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.3,
+      }
+    },
+    delay: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.3,
+        delay: 0.3
+      }
+    },
+  };
+
   const SkillsItems = userProps.stack.join(', ')
 
   const personalInfo = (
@@ -42,20 +61,39 @@ export default function AboutMe({ }: Props) {
   )
   return (
     <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ duration: 0.8 }}
       className="flex flex-col mt-16 md:px-6 mx-4 max-w-4xl items-center justify-center md:w-full"
     >
-      <div className="m-8 md:w-full">
+      <motion.div
+        variants={item}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        className="m-8 md:w-full"
+      >
         <h1 className="text-3xl font-semibold pb-4 tracking-widest">Sobre mim</h1>
         <h2 className="text-blue-400 font-semibold text-xl mb-4">Informações Básicas</h2>
         {personalInfo}
-      </div>
-      <div className="m-8 md:w-full">
+      </motion.div>
+      <motion.div
+        variants={item}
+        initial="hidden"
+        whileInView="delay"
+        viewport={{ once: true }}
+        className="m-8 md:w-full"
+      >
         <h2 className="text-blue-400 font-semibold text-xl mb-4">Olá, é um prazer te conhecer!</h2>
         <p className="text-zinc-400">{userProps.moreAbout}</p>
-      </div>
+      </motion.div>
+      <motion.div
+        variants={item}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+        className="m-8 md:w-full"
+      >
+        <h2 className="text-blue-400 font-semibold text-xl mb-4">Olá, é um prazer te conhecer!</h2>
+        <p className="text-zinc-400">{userProps.moreAbout}</p>
+      </motion.div>
     </motion.div>
 
   )
