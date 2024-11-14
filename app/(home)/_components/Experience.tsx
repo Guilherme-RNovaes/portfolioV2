950090d6b31c5bb033fa1ff45e8eeabb52afe401
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-11-14 19:36:23 -0300

    fix: fix experience container padding

--- app/(home)/_components/Experience.tsx
+++ app/(home)/_components/Experience.tsx
@@ -27,7 +27,7 @@ const item = {
 export default function Experience({ }: Props) {
   const experiences = experiencesList.map((props) => {
     return (
-      <motion.div key={props.company} variants={item} initial="hidden" whileInView="visible" transition={{ duration: 0.5 }} className="flex flex-row items-center gap-4 mb-4 md:mb-0 py-3 md:p-3">
+      <motion.div key={props.company} variants={item} initial="hidden" whileInView="visible" transition={{ duration: 0.5 }} className="min-w-[250px] flex flex-row items-center gap-4 mb-4 md:mb-0 py-3 md:p-3">
         <Avatar className="bg-white text-black flex justify-center">
           <AvatarImage src={props.image} alt="Imagem da empresa" />
           <AvatarFallback className="mt-2"><Image /></AvatarFallback>
@@ -56,7 +56,7 @@ export default function Experience({ }: Props) {
         <h3 className="uppercase tracking-widest font-semibold text-xl pb-4">
           Experiencia
         </h3>
-        <div className="flex flex-col md:grid md:grid-cols-2 gap-4 md:px-32 md:pb-4 md:gap-x-20 mx-10 md:mx-4 md:max-w-4xl justify-center md:justify-start items-start md:items-center">
+        <div className="flex flex-col md:grid md:grid-cols-2 gap-4 md:px-28 md:pb-4 md:gap-x-20 mx-10 md:mx-4 md:max-w-4xl justify-center md:justify-start items-start md:items-center">
           {experiences}
         </div>
       </div>
