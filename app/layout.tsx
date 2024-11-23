e19b29defa0638dfa015c3d9b65c278804c27bc8
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-11-23 10:35:41 -0300

    style: change default app font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
 import { Metadata } from 'next'
-import { Inter } from "next/font/google"
+import { Lexend } from "next/font/google"
 import "./globals.css"
 import Header from './_components/Header';
 import ScrollToTopButton from './_components/ScrollToTopButton';
 
-const inter = Inter({ subsets: ["latin"] });
+const googleFont = Lexend({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+});
 
 export const metadata: Metadata = {
   title: 'Guilherme Novaes | Portfolio',
@@ -20,7 +23,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={`${inter.className} dark relative h-full w-full pb-10 bg-slate-950 text-white`}>
+      <body className={`${googleFont.className} dark relative h-full w-full pb-10 bg-slate-950 text-white`}>
         <div className="absolute z-0 bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#0f172a80_1px,transparent_1px),linear-gradient(to_bottom,#0f172a80_1px,transparent_1px)] bg-[size:14px_14px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]" />
         <Header />
         {children}
