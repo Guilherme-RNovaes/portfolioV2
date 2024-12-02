cd9dea55733476703bc952df253cdaced9f5e758
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-12-02 10:41:18 -0300

    style: change shadcn sheet style

--- app/_components/ui/sheet.tsx
+++ app/_components/ui/sheet.tsx
@@ -29,7 +29,7 @@ const SheetOverlay = React.forwardRef<
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName
 
 const sheetVariants = cva(
-  "fixed z-50 gap-4 bg-white p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 dark:bg-slate-950/30 backdrop-blur-xl",
+  "fixed z-50 gap-4 bg-white p-6 shadow-lg transition ease-in-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:duration-500 dark:bg-black/30 backdrop-blur-xl",
   {
     variants: {
       side: {
